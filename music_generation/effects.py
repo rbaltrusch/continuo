774b54f2b1710a5556b39b8da76e43f758fbe267
Richard Baltrusch <richard@baltrusch.net>
2022-05-08 17:07:06 +0200

    Add typehints

--- music_generation/effects.py
+++ music_generation/effects.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 """Wrapper around the musical.audio.effect module"""
 
+import numpy
 from musical.audio import effect
 
-def apply_effects(data):
+def apply_effects(data: numpy.ndarray) -> numpy.ndarray:
     """Takes in a numpy array and applies sound effects"""
     return effect.flanger(effect.tremolo(effect.chorus(data, 2), 4), 4)
