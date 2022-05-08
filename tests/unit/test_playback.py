bcba5a54413ad1d977b7c39e8aa0f0e92c7eba30
Richard Baltrusch <richard@baltrusch.net>
2022-05-08 19:28:54 +0200

    Deselect playback test for workflow

--- tests/unit/test_playback.py
+++ tests/unit/test_playback.py
@@ -2,7 +2,9 @@
 """Playback module tests"""
 
 import numpy
+import pytest
 from music_generation import playback
 
+@pytest.mark.skip_workflow
 def test_playback():
     playback.play(data=numpy.ndarray((1, 2)))
